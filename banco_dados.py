4d24ae51e5a411ef9595139313e271b086130b4e
Far <lqdevelop01@gmail.com>
2025-07-08 23:28:26 -0400

    MODIFICANDO UMAS COISAS

--- banco_dados.py
+++ banco_dados.py
@@ -2,28 +2,28 @@ import psycopg2
 
 conexao = psycopg2.connect(
             dbname="DBJulhoExperimento",
-            user="postgres",
-            password="luique1810",
-            host="localhost",
+            user="experimentodb_user",
+            password="QpZyNprNbfLSB3DF613YiRb9dulneftu",
+            host="dpg-d1mu0mre5dus7388ul60-a",
             port="5432",
         )
 
 
-def criarBanco():
-    with conexao as conn:
 
-        # Open a cursor to perform database operations
-        with conn.cursor() as cur:
+with conexao as conn:
 
-            # Execute a command: this creates a new table
-            cur.execute("""
-                CREATE TABLE test (
-                    id serial PRIMARY KEY,
-                    nome text)
-                """)
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
 
-            # Make the changes to the database persistent
-            conn.commit()
+        # Execute a command: this creates a new table
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS test (
+                id serial PRIMARY KEY,
+                nome text)
+            """)
+
+        # Make the changes to the database persistent
+        conn.commit()
 
 def registrarNome(nome):
 
