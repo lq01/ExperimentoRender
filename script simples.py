78d3b6d2b9a726e64a657a35f7350a0431fcf455
Far <lqdevelop01@gmail.com>
2025-07-08 23:55:56 -0400

    serviço flask

--- script simples.py
+++ script simples.py
@@ -31,3 +31,8 @@ def cadastrarNome():
     if request.method == 'POST':
         banco_dados.registrarNome(request.form['nome'])
         return "Nome cadastrado"
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
